c60c3af0f65cc3455b706c2adce36e2300240c90
AliAtto <aaaf1318@gmail.com>
2024-08-20 21:42:11 +0300

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn }) => {
   let caption = `
 *「 معلومات عن صاحب البوت 」*
 
-*Number :*\ALI_ATTO_0988109204
+*Number :*\Ali Atto_0988109204
 *instagram:*\ninstagram.com/https://www.instagram.com/ali_m_atto?igsh=bjNiNWRqYjY2d2t0
 
 
-*script bot :* github.com/ALIATTO 
+*script bot :* github.com/AliAtto
 
 `.trim()
   m.reply(caption)
